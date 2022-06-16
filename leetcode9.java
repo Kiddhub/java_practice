4267bf60c59835b55b51104333750a8d94cd048e
OdinIT <quangminh2001.hust@gmail.com>
2022-06-16 13:31:52 +0700

    Update leetcode9.java

--- leetcode9.java
+++ leetcode9.java
@@ -1,32 +1,33 @@
-public class leetcode9 {
-        public static void main (String[] args){
-            
+public class leetcode9{
+    public static void main(String[] args){
+        int x = 121;
+        System.out.println(isPalindrome(x));
+    }
+    public static boolean isPalindrome(int x) {
+        if (x<0){
+            return false;
         }
-        public boolean isPalindrome(int x) {
-            if (x<0){
-                return false;
+        else if (x==0){
+            return true;
+        }
+        else if (x>0){
+            int n = x;
+            int res = 0;
+            int tmp;
+            while(n>0){
+                tmp = n % 10;
+                res = res*10 + tmp;
+                n = n/10;
             }
-            else if (x==0){
+            if (res == x)
+            {
                 return true;
             }
-            else if (x>0){
-                int n = x;
-                int res = 0;
-                int tmp;
-                while(n>0){
-                    tmp = n % 10;
-                    res = res*10 + tmp;
-                    n = n/10;
-                }
-                if (res == x)
-                {
-                    return true;
-                }
-                else {
-                    return false;
-                }
+            else {
+                return false;
             }
-            return false;
-
         }
+        return false;
+
+    }
 }
