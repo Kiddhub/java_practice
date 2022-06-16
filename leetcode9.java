767ce054a46dcc4c94f3a4d0f2812ef3581e8498
OdinIT <quangminh2001.hust@gmail.com>
2022-06-16 13:12:30 +0700

    9. Palindrome Number

---
+++ leetcode9.java
@@ -0,0 +1,32 @@
+public class leetcode9 {
+        public static void main (String[] args){
+            
+        }
+        public boolean isPalindrome(int x) {
+            if (x<0){
+                return false;
+            }
+            else if (x==0){
+                return true;
+            }
+            else if (x>0){
+                int n = x;
+                int res = 0;
+                int tmp;
+                while(n>0){
+                    tmp = n % 10;
+                    res = res*10 + tmp;
+                    n = n/10;
+                }
+                if (res == x)
+                {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            return false;
+
+        }
+}
