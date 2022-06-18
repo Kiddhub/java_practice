f0eee46737ccb2bf7981ae1ec8056e430cf1a0c5
OdinIT <quangminh2001.hust@gmail.com>
2022-06-18 10:58:32 +0700

    Create leetcode14.java
    Longest Common Prefix

---
+++ leetcode14.java
@@ -0,0 +1,21 @@
+// Longest Common Prefix
+public class leetcode14 {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0)
+            return "";
+        String prefix = strs[0];
+        for (int i = 0;i<strs.length;i++){
+            while (strs[i].indexOf(prefix) !=0){
+                prefix =prefix.substring(0,prefix.length()-1);
+            }
+        }
+        return prefix;
+    }
+    public static void main(String[] args){
+        String[] a = {"flower","flow","flight"};
+        leetcode14 result = new leetcode14();
+        System.out.println(result.longestCommonPrefix(a));
+    }
+
+}
+
