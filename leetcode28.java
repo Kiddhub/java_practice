665b4264de29289932857be615dea9e94d1991a4
OdinIT <quangminh2001.hust@gmail.com>
2022-06-22 22:31:19 +0700

    28. Implement strStr()

---
+++ leetcode28.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/implement-strstr/
+import java.util.*;
+class leetcode28 {
+    public int strStr(String haystack, String needle) {
+        if (needle.isEmpty())
+            return 0;
+        int m = haystack.length();
+        int n = needle.length();
+        if (m<n)
+            return -1;
+        for (int i =0;i<=m-n;++i){
+            int j;
+            for (j=0;j<n;j++){
+                if (haystack.charAt(i+j)!=needle.charAt(j)){
+                    break;
+                }
+
+            }
+            if (j==n)
+                return i;
+        }
+        return -1;
+
+    }
+}
